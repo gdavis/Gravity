a812db3a020de59a6b6b7d60ef9c0c169668b365
Grant Davis <grant.davis@gmail.com>
2013-12-15 10:12:00 -0700

    make use of existing method

--- Gravity/UIColor+GDIAdditions.m
+++ Gravity/UIColor+GDIAdditions.m
@@ -29,9 +29,7 @@
 
 + (UIColor *)colorWithRGBHex:(uint)hex
 {
-    return [UIColor colorWithRed:((float)((hex & 0xFF0000) >> 16))/255.0 
-                           green:((float)((hex & 0xFF00) >> 8))/255.0 
-                            blue:((float)(hex & 0xFF))/255.0 alpha:1.0];
+    return [UIColor colorWithRGBHex:hex alpha:1.f];
 }
 
 + (UIColor *)colorWithRGBHex:(uint)hex alpha:(CGFloat)alpha
